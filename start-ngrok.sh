09d5a4a628def23a66a423b6c73dbc93ab919afd
Kayla Altepeter <kayla@kaylaaltepeter.com>
2019-02-03 15:07:07 -0600

    Update start-ngrok.sh

--- start-ngrok.sh
+++ start-ngrok.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+[[ ${DEBUG:-} == true ]] && set -o xtrace
+readonly __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+(command -v ngrok) || bash "${__dir}/get-ngrok.sh"
+
 # start ngrok
 readonly ngrok_pid="${__dir}/ngrok-pid.txt"
 ps -p "$(cat "${ngrok_pid}")" && kill -9 "$(cat "${ngrok_pid}")"
